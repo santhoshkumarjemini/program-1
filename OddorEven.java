3a57341ff7f68c45951db404c8dffd2603566f00
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-03-26 18:27:45 +0530

    OddorEven.java

---
+++ OddorEven.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class OddorEven
+{
+int a;
+Scanner s=new Scanner();
+a=s.nextInt;
+if(a%2!=0)
+{
+System.out.println("Given number is Odd");
+else
+System.out.println("Given number is Even");
+}
+}
