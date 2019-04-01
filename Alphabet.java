4c87eea730e6cc5bbf8b12434ce711c924bbaa47
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-04-01 20:09:23 +0530

    Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Alphabet
+{
+ public static void main(String args[])
+  {
+   ch='*';
+   if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+   {
+    System.out.println(ch+ "is an alphabet");
+   else
+    System.out.println(ch+ "is not an alphabet");
+   }
+  }
+}
