478dadbc7ee86558294c18bb7f6f40b1f5a8c909
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-04-03 20:19:37 +0530

    Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Helloworld
+{
+ public static void main(String args[])
+  {
+   int n;
+   Scanner s=new Scanner(System.in);
+   System.out.println("Enter the n value");
+    for(int i=0;i<n;i++)
+    {
+     System.out.println("Hello World");
+    }
+  }
+}
