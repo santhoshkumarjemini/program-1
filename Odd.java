b2d702e9e41c6830a49414c3caf129747c226435
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-04-17 00:10:00 -0700

    Odd.java

---
+++ Odd.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Odd
+{
+ public static void main(String args[])
+  {
+   int n1,n2;
+   System.out.println("Enter the number");
+   Scanner s=new Scanner(System.in);
+   n1=s.nextInt();
+   n2=s.nextInt();
+    while(n1<=n2)
+    {
+     if(n1%2==0)
+      {
+       System.out.println(n1+ " ");
+      }
+     n1++;
+    }
+  }
+}
