ee495664b362078c73bf1afb6889f755fdf525de
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-04-21 02:32:45 -0700

    Index.java

---
+++ Index.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Index
+{
+ public static void main(String args[])
+ {
+  int n;
+  System.out.println("Enter the input");
+  Scanner s=new Scanner(System.in);
+  n=s.nextInt();
+  int a[]=new int[];
+  for(int i=0;i<n;i++)
+  {
+   a[i]=s.nextInt();
+  }
+  for(int i=0;i<n;i++)
+  {
+   System.out.println(a[i]+" "+i);
+  }
+ }
+}
