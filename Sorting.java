2e398c62c62cc40e32027f068c16517b328b6060
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-04-23 16:05:08 +0530

    Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -6,6 +6,25 @@ public class Sort
  {
   int n;
   System.out.println("Enter the Input");
+  Scanner s=new Scanner(System.in);
+  n=s.nextInt();
+  int a[]=new int[n];
+   for(int i=0;i<n;i++)
+   {
+     a[i]=s.nextInt();
+   }
+   for(int i=0;i<n;i++)
+   {
+     for(int j=i+1;j<n;j++)
+     {
+       if(a[i]>a[j])
+       {
+         int temp=a[i];
+        a[i]=a[j];
+        a[j]=a[i];
+       }
+     }
+   }
   
  }
 }
