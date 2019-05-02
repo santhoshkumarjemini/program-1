52f7b6fe41c40e6f8b06a7ba8439f708f536447a
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-02 09:12:20 -0700

    Prt.java

---
+++ Prt.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Prt
+{
+ public static void main(String args[])
+ {
+  int n;
+  String s;
+  Scanner in=new Scanner(System.in);
+  s=in.nextLine();
+  n=in.nextInt();
+   for(int i=0;i<n;i++)
+   {
+   System.out.println(s);
+   }
+ }
+}
