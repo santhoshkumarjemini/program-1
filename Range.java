13af851488e07bda7b2ca312c7a488e857a30078
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-05 00:21:32 -0700

    Range.java

---
+++ Range.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Range
+{
+ public static void main(String args[])
+ {
+  int i;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  i=s.nextInt();
+   if((i>=1)&&(i<=10))
+   {
+    System.out.println("Yes");
+   }
+ }
+}
