76f30fcb11be2288015ff92ed518b5b14e9bdbc6
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-05 06:58:23 -0700

    Print.java

---
+++ Print.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Print
+{
+ public static void main(String args[])
+ {
+  int num;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  num=s.nextInt();
+  num=num+1;
+  System.out.println(num);
+ }
+}
