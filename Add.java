a5492fe6b0d2fc7f0726a7d31d05ff9e167e8d2d
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-06 07:07:39 -0700

    Add.java

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Add
+{
+ public static void main(String args[])
+ {
+  int a,b,c;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  a=s.nextInt();
+  b=s.nextInt();
+  c=a+b;
+  System.out.println(c);
+ }
+}
