f3ebc563cd91849286349dbff44d989429e11b2c
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-09 07:03:30 -0700

    Printfirst.java

--- Printfirst.java
+++ Printfirst.java
@@ -4,13 +4,13 @@ public class Printfirst
 {
  public static void main(String args[])
  {
- int n;
- String str;
+ int k;
+ String s;
  System.out.println("Input");
- Scanner s=new Scanner(System.in);
- str=s.nextLine();
- n=s.nextInt();
-  for(int i=0;i<n;i++)
+ Scanner in=new Scanner(System.in);
+ s=in.nextLine();
+ k=in.nextInt();
+  for(int i=0;i<k;i++)
   {
    System.out.println(str.charAt(i));
   }
