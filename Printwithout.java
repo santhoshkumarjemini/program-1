128a222cc7bfd28f35522ac26a92780b23086d1d
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-10 13:28:03 +0530

    Printwithout.java

---
+++ Printwithout.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Printwithout
+{
+ public static void main(String args[])
+ {
+  String str;
+  Scanner s=new Scanner(System.in);
+  str=s.nextLine();
+   for(int i=1;i<2;System.out.println(str))
+   {
+    i++;
+   }
+ }
+}
