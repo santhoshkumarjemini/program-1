a4ca1ff02ea201ddbac11213629bfc9c394e5a08
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-11 00:42:01 -0700

    Round.java

---
+++ Round.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Round
+{
+ public static void main(String args[])
+ {
+  double a;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  a=s.nextDouble();
+  System.out.println(Math.ceil(a));
+ }
+}
