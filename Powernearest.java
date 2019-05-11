ad89dfc6852b573d0003dec4d20669b6942a68ea
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-10 19:55:55 -0700

    Powernearest.java

---
+++ Powernearest.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Powernearest
+{
+ public static void main(String args[])
+ {
+  int n,res;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  n=s.nextInt();
+  res=n*2;
+  System.out.println(res);
+ }
+}
