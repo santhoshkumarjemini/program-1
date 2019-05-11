c895da10eac1f643591d8c7b55448ce41200383d
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-10 19:45:27 -0700

    Difference.java

---
+++ Difference.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Difference
+{
+ public static void main(String args[])
+ {
+  int a,b,c;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  a=s.nextInt();
+  b=s.nextInt();
+  c=a-b;
+   if(c%2==0)
+   {
+    System.out.println("Even");
+   }
+   else
+   {
+    System.out.println("Odd");
+   }
+ }
+}
