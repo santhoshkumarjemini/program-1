e658b9658119db2729c2c3733c254b87e03373cd
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-12 07:45:13 -0700

    Squres.java

--- Squres.java
+++ Squres.java
@@ -4,12 +4,11 @@ public class Squres
 {
  public static void main(String args[])
  {
-  int n,k,res=0;
+  int n,k;
   System.out.println("Input");
   Scanner in=new Scanner(System.in);
   n=in.nextInt();
   k=in.nextInt();
-  res=n^k;
-  System.out.println(res);
+  System.out.println(Math.round(Math.pow(n,k)));
  }
 }
