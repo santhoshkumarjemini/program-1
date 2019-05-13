8f374e4c83cff5d5cd8a56cd9f5ba8a7d97f08ed
santhoshkumarjemini <48647898+santhoshkumarjemini@users.noreply.github.com>
2019-05-13 15:23:01 +0530

    Basicmaths.java

---
+++ Basicmaths.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Basicmaths
+{
+ public static void main(String args[])
+ {
+  int a,b,c;
+  System.out.println("Input");
+  Scanner s=new Scanner(System.in);
+  a=s.nextInt();
+  b=s.nextInt();
+  c=s.nextInt();
+  System.out.println((a*b)/c);
+ }
+}
